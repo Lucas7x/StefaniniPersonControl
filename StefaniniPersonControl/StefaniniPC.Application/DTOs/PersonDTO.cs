7adc00b209fb7263c4f22b7c36df110c6167ec08
Lucas Xavier <lukasx1934@gmail.com>
2025-08-20 15:04:34 -0300

    feat: adding PersonDTO to Application project

---
+++ StefaniniPersonControl/StefaniniPC.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StefaniniPC.Application.DTOs
+{
+    public class PersonDTO
+    {
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        public string Name { get; set; } = string.Empty;
+        public string? Gender { get; set; }
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "A Data de Nascimento é obrigatória")]
+        public DateTime BirthDate { get; set; } = DateTime.MinValue;
+        public string? Nationality { get; set; }
+        public string? PlaceOfBirth { get; set; }
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        public string Cpf { get; set; } = string.Empty;
+    }
+}
