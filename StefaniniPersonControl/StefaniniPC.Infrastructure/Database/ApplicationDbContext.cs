ae3f89b7c455e05c601060fe4ae85d1829f2e3a6
Lucas Xavier <lukasx1934@gmail.com>
2025-08-20 15:39:55 -0300

    feat: adding ApplicationDbContext

---
+++ StefaniniPersonControl/StefaniniPC.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StefaniniPC.Domain.Entities;
+
+namespace StefaniniPC.Infrastructure.Database
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Person> Persons { get; private set; }
+    }
+}
