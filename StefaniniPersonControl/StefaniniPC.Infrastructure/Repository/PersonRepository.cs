a7b7ac69cedb8c5c8ab9aa410d35733f8f9c21b6
Lucas Xavier <lukasx1934@gmail.com>
2025-08-20 15:40:39 -0300

    feat: adding PersonRepository in Infrastructure project

---
+++ StefaniniPersonControl/StefaniniPC.Infrastructure/Repository/PersonRepository.cs
@@ -0,0 +1,42 @@
+﻿using StefaniniPC.Application.Filters;
+using StefaniniPC.Application.Interfaces;
+using StefaniniPC.Domain.Entities;
+using StefaniniPC.Infrastructure.Database;
+
+namespace StefaniniPC.Infrastructure.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PersonRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Person?> GetPersonByIdAsync(long id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> ListPersonAsync(PersonQueryFilter filter, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreatePersonAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePersonAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePersonAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
